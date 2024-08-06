fc83cf20f5c17887f4e3eac93097544bcc46fdba
Daveed <daveed@santocabron.com>
2024-08-06 18:49:50 -0400

    copied files from the medplum/package

--- README.md
+++ README.md
@@ -8,4 +8,38 @@ Supports the [medplum api](https://api.medplum.com/), in the enterprise plans an
 
 Using NEXTJS to offer API, bot and front end components and extensions.
 
-Open source. Poke around our demo version [ehr.calimania.org](https://ehr.calimania.org) and contact our team for support, custom integrations and enterprise plans.
+## Open source.
+
+Released under (TSL). Full [./LICENSE] text.
+
+Explore some features in our demo version [ehr.calimania.org](https://ehr.calimania.org).
+
+Contact our team for support, custom integrations and enterprise plans.
+
+This project was forked from [medplum/app](https://app.medplum.com)
+
+## Config Settings
+
+When building and running the app from source, config settings are loaded from environment variables or the `.env` file.
+
+| Name                       | Description                                                                                | Required |
+| -------------------------- | ------------------------------------------------------------------------------------------ | -------- |
+| `MEDPLUM_BASE_URL`         | The base URL of the API server for all API calls. For example, "https://api.medplum.com/". | yes      |
+| `MEDPLUM_CLIENT_ID`        | Optional Medplum client application ID.                                                    | no       |
+| `GOOGLE_CLIENT_ID`         | Optional Google client application ID for Google Auth.                                     | no       |
+| `RECAPTCHA_SITE_KEY`       | Optional reCAPTCHA site key for reCAPTCHA user verification.                               | no       |
+| `MEDPLUM_REGISTER_ENABLED` | Optional flag to enable or disable open registration for new projects.                     | no       |
+
+## Developing
+
+Dev server:
+
+```bash
+npm run dev
+```
+
+Production build:
+
+```bash
+npm run build
+```
---
+++ src/App.tsx
@@ -0,0 +1,104 @@
+import { Space } from '@mantine/core';
+import { MEDPLUM_VERSION } from '@medplum/core';
+import { UserConfiguration } from '@medplum/fhirtypes';
+import { AppShell, Loading, NavbarMenu, useMedplum } from '@medplum/react';
+import {
+  IconBrandAsana,
+  IconBuilding,
+  IconForms,
+  IconId,
+  IconLock,
+  IconLockAccess,
+  IconMicroscope,
+  IconPackages,
+  IconReceipt,
+  IconReportMedical,
+  IconStar,
+  IconWebhook,
+} from '@tabler/icons-react';
+import { FunctionComponent, Suspense } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
+import { AppRoutes } from './AppRoutes';
+
+import './App.css';
+
+export function App(): JSX.Element {
+  const medplum = useMedplum();
+  const config = medplum.getUserConfiguration();
+  const location = useLocation();
+  const [searchParams] = useSearchParams();
+
+  const Logo = '/static/img/calima-logo-512x512.png';
+
+  if (medplum.isLoading()) {
+    return <Loading />;
+  }
+
+  return (
+    <AppShell
+      logo={<img src={Logo} width={24} title="Calima Logo" />}
+      pathname={location.pathname}
+      searchParams={searchParams}
+      version={MEDPLUM_VERSION}
+      menus={userConfigToMenu(config)}
+      displayAddBookmark={!!config?.id}
+    >
+      <Suspense fallback={<Loading />}>
+        <AppRoutes />
+      </Suspense>
+    </AppShell>
+  );
+}
+
+function userConfigToMenu(config: UserConfiguration | undefined): NavbarMenu[] {
+  const result =
+    config?.menu?.map((menu) => ({
+      title: menu.title,
+      links:
+        menu.link?.map((link) => ({
+          label: link.name,
+          href: link.target as string,
+          icon: getIcon(link.target as string),
+        })) || [],
+    })) || [];
+
+  result.push({
+    title: 'Settings',
+    links: [
+      {
+        label: 'Security',
+        href: '/security',
+        icon: <IconLock />,
+      },
+    ],
+  });
+
+  return result;
+}
+
+const resourceTypeToIcon: Record<string, FunctionComponent> = {
+  Patient: IconStar,
+  Practitioner: IconId,
+  Organization: IconBuilding,
+  ServiceRequest: IconReceipt,
+  DiagnosticReport: IconReportMedical,
+  Questionnaire: IconForms,
+  admin: IconBrandAsana,
+  AccessPolicy: IconLockAccess,
+  Subscription: IconWebhook,
+  batch: IconPackages,
+  Observation: IconMicroscope,
+};
+
+function getIcon(to: string): JSX.Element | undefined {
+  try {
+    const resourceType = new URL(to, 'https://app.medplum.com').pathname.split('/')[1];
+    if (resourceType in resourceTypeToIcon) {
+      const Icon = resourceTypeToIcon[resourceType];
+      return <Icon />;
+    }
+  } catch (_err) {
+    // Ignore
+  }
+  return <Space w={30} />;
+}
